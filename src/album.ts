bb1725cbb1bb4ac6254a7eb46b948e7df5d5f966
tregdor <49729985+tregdor@users.noreply.github.com>
2020-04-24 13:40:55 +0300

    rewrote class methods

--- src/album.ts
+++ src/album.ts
@@ -1,3 +1,5 @@
+
+
 interface Image {
   albumId: number;
   id: number;
@@ -14,14 +16,16 @@ export class Album {
   private previousButton: HTMLButtonElement;
   private nextButton: HTMLButtonElement;
   private currentPage: number;
+  public localStorageKey: string;
 
   constructor(containerSelector: string) {
+    this.localStorageKey = "albumData";
     this.albumContainer = document.querySelector(containerSelector);
     this.currentPage = 1;
     this.renderImagesContainer();
     this.renderButtons();
     this.renderAlbumTitle();
-    const localData = this.getDataFromLocalStorage();
+    const localData = this.getDataFromLocalStorage(this.localStorageKey);
 
     if (localData) {
       this.album = localData;
@@ -38,9 +42,9 @@ export class Album {
       .then((response) => response.json())
       .then((album) => {
         this.album = album;
-        this.setDataToLocalStorage();
+        this.setDataToLocalStorage(this.localStorageKey,this.album);
         this.renderImages();
-        this.disableButton(false);
+        this.disableButton(false, this.previousButton, this.nextButton);
       })
       .catch((error) => console.error(error));
   }
@@ -54,7 +58,10 @@ export class Album {
 
   private renderImages(): void {
     if (document.querySelector(".album__image")) {
-      this.changeImageInDocument(this.album);
+      this.changeImageInDocument(
+        Array.from(document.querySelectorAll(".album__image")),
+        this.album,
+      );
     } else {
       this.album.map((image) => {
         const img: HTMLImageElement = document.createElement("img");
@@ -68,12 +75,10 @@ export class Album {
     }
   }
 
-  private changeImageInDocument(album: Image[]): void {
-    Array.from(document.querySelectorAll(".album__image")).map(
-      (image: HTMLImageElement, index: number) => {
-        image.src = album[index].url;
-      },
-    );
+  private changeImageInDocument(albumImages: HTMLImageElement[], albumData: Image[]): void {
+    albumImages.map((image: HTMLImageElement, index: number) => {
+      image.src = albumData[index].url;
+    });
   }
 
   private renderImagesContainer(): void {
@@ -94,60 +99,63 @@ export class Album {
     this.previousButton = document.createElement("button");
     this.previousButton.classList.add("album__left-button");
     this.previousButton.textContent = "<";
-    this.previousButton.addEventListener("click", () =>
-      this.selectNewPage("previous"),
-    );
+    this.previousButton.addEventListener("click", () => {
+      this.currentPage = this.selectNewPage("previous", this.currentPage);
+      this.fetchAlbum(this.currentPage);
+    });
 
     this.nextButton = document.createElement("button");
     this.nextButton.classList.add("album__right-button");
-    this.nextButton.addEventListener("click", () => this.selectNewPage("next"));
+    this.nextButton.addEventListener("click", () => {
+      this.currentPage = this.selectNewPage("next", this.currentPage);
+      this.fetchAlbum(this.currentPage);
+    });
     this.nextButton.textContent = ">";
 
     this.albumContainer.append(this.previousButton);
     this.albumContainer.append(this.nextButton);
   }
 
-  private disableButton(isDisable: boolean): void {
-    this.previousButton.disabled = isDisable;
-    this.nextButton.disabled = isDisable;
+  public disableButton(isDisable: boolean, previousButton: HTMLButtonElement, nextButton: HTMLButtonElement,): void {
+    previousButton.disabled = isDisable;
+    nextButton.disabled = isDisable;
   }
 
-  private selectNewPage(direction: string): void {
-    this.disableButton(true);
+  public selectNewPage(direction: string, currentPage: number): number {
+    this.disableButton(true, this.previousButton, this.nextButton);
     this.scrollToTop();
+
     if (direction === "previous") {
-      if (this.currentPage === 1) {
-        return;
+      if (currentPage === 1) {
+        return 100;
       }
-      this.currentPage -= 1;
-      this.fetchAlbum(this.currentPage);
+      return currentPage -= 1;
     } else {
-      if (this.currentPage === 100) {
-        return;
+      if (currentPage === 100) {
+        return 1;
       }
-      this.currentPage += 1;
-      this.fetchAlbum(this.currentPage);
+      return currentPage += 1;
     }
   }
 
-  private setDataToLocalStorage(): void {
-    localStorage.removeItem("albumData");
-    localStorage.setItem("albumData", JSON.stringify(this.album));
+  public setDataToLocalStorage(key: string, data: Image[]): void {
+    if(localStorage.getItem(key)){
+      localStorage.removeItem(key);
+    }
+    localStorage.setItem(key, JSON.stringify(data));
   }
 
-  private getDataFromLocalStorage(): Image[] {
-    return JSON.parse(localStorage.getItem("albumData"));
+  public getDataFromLocalStorage(key: string): Image[] {
+    return JSON.parse(localStorage.getItem(key));
   }
 
   private fullscreenImage(img: Image): void {
-    this.previousButton.disabled = true;
-    this.nextButton.disabled = true;
+    this.disableButton(true,this.previousButton,this.nextButton);
     const image: HTMLImageElement = document.createElement("img");
     image.src = img.url;
     image.classList.add("full-screen");
     image.addEventListener("click", () => {
-      this.previousButton.disabled = false;
-      this.nextButton.disabled = false;
+      this.disableButton(false,this.previousButton,this.nextButton);
       image.remove();
     });
     this.albumContainer.append(image);
